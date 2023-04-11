cd1843bd518f0616024a63c92c5ca93d2b170920
iWpoo <98208516+iWpoo@users.noreply.github.com>
2023-04-11 21:08:30 +0600

    Update Controller.php

--- core/Controller.php
+++ core/Controller.php
@@ -7,38 +7,40 @@ use Twig\Environment;
 class Controller
 {	
 	// Render the twig pages	
-	protected function render($view, $data = []) {
+	protected function render($view, $data = [])
+	{
 		$viewPath = $_SERVER['DOCUMENT_ROOT'] . "/project/views/";
-        $loader = new FilesystemLoader($viewPath);
-        $twig = new Environment($loader);
+		$loader = new FilesystemLoader($viewPath);
+		$twig = new Environment($loader);
 
 		return $twig->render($view, $data);
 	}
     
 	// JSON
-	protected function response($data) {
-        $json = json_encode($data);
-        header('Content-Type: application/json');
-        return $json;
+	protected function response($data)
+	{
+		$json = json_encode($data);
+		header('Content-Type: application/json');
+		return $json;
 	}
     
 	// Add post element with xss defence
 	protected function add($element, string $way = 'htmlspecialchars')
 	{
 		if ($way === 'htmlspecialchars') {
-            return htmlspecialchars(trim($_POST[$element]));
+			return htmlspecialchars(trim($_POST[$element]));
 		} else if ($way === 'strip_tags') {
 			return strip_tags(trim($_POST[$element]));
 		}
 	}
 
-    // Upload files
+        // Upload files
 	protected function uploadFile($file, $filename, $path)
 	{
 		$allPath = '/project/webroot/' . $path . '/';
 		$destination = $_SERVER['DOCUMENT_ROOT'] . $allPath . $filename;
 		move_uploaded_file($file['tmp_name'], $destination);
-    }
+	}
 
 	protected function uniqueNameFile($file, $allowed_extensions = ["png", "jpg", "mp4", "gif", "jpeg"])
 	{
@@ -50,7 +52,7 @@ class Controller
 	// Redirect
 	protected function redirect($to)
 	{
-        header("Location: $to");
+		header("Location: $to");
 		return $this;
 	}
 
@@ -62,18 +64,19 @@ class Controller
 	
 		// Отправляем HTTP-заголовок с кодом 302 и URL-адресом предыдущей страницы
 		header("Location: $url", true, 302);
-        return $this;
+		return $this;
 	}
 	
 
 	// CSRF token
-	protected function generateCsrfToken() {
-        if(!isset($_SESSION['csrf_token'])){
-            $token = bin2hex(random_bytes(32));
-            $_SESSION['csrf_token'] = $token;
-        } else {
-		    $token = $_SESSION['csrf_token'];
+	protected function generateCsrfToken()
+	{
+		if (!isset($_SESSION['csrf_token'])) {
+			$token = bin2hex(random_bytes(32));
+			$_SESSION['csrf_token'] = $token;
+		} else {
+			$token = $_SESSION['csrf_token'];
 		}
-        return $token;
-    }
+		return $token;
+	}
 }

b5e7b926ea2c2544b31177ec578ff8d5a8297d99
iWpoo <98208516+iWpoo@users.noreply.github.com>
2023-04-22 19:52:47 +0600

    Update CartController.php

--- project/controllers/CartController.php
+++ project/controllers/CartController.php
@@ -43,7 +43,7 @@ class CartController extends Controller
 
             // Отправка данных на клиент в формате JSON
             setcookie('cart_qty', $cart_qty, $cart_qty == 0 ? time() - 86400 * 30 : time() + 86400 * 30, '/');            
-	        $data = ['csrf_token' => $this->generateCsrfToken(), 'countCart' => $cart_qty];
+	    $data = ['csrf_token' => $this->generateCsrfToken(), 'countCart' => $cart_qty];
             return json_encode($data);
         }
     }
